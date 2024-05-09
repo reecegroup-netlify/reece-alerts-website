ddc0a82b13ad265879748acae0e6462a6a2380b6
Adrian Beyerle <adrian.beyerle@reece.com.au>
2024-05-09 14:25:50 +1000

    add back button logic

---
+++ components/back-button.js
@@ -0,0 +1,29 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { ArrowLeftIcon } from "@heroicons/react/20/solid";
+
+const defaultFallback = "/"; // Default fallback path if no history is present
+
+export default function BackButton({ fallback = defaultFallback }) {
+  const router = useRouter();
+
+  function handleBack() {
+    if (window.history.length > 1) {
+      router.back();
+    } else {
+      router.push(fallback | "/");
+    }
+  }
+
+  return (
+    <button
+      type="button"
+      className="border rounded-lg px-4 py-2 hover:text-sky-500 dark:hover:text-sky-400 flex items-center space-x-2"
+      onClick={handleBack}
+    >
+      <ArrowLeftIcon className="w-4 h-4" />
+      <span>Back</span>
+    </button>
+  );
+}
