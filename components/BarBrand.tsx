df4b89cdaa50acd98ade33f8ce19f10662d0a8c6
Adrian Beyerle <adrian.beyerle@reece.com.au>
2024-06-03 13:10:30 +1000

    performance issues due to header bar

--- components/BarBrand.tsx
+++ components/BarBrand.tsx
@@ -3,14 +3,9 @@ import siteBrandBar from 'media/site-brand-bar@2x.png'
 
 export default function BarBrand() {
   return (
-    <div className="h-[15px] overflow-hidden sm:h-[20px] md:h-[25px] lg:h-[30px]">
-      <Image
-        src={siteBrandBar}
-        width={3580}
-        height={60}
-        className="h-full w-auto min-w-full max-w-none"
-        alt="Pattern of vertical stripes in navy blue and light grey"
-      />
-    </div>
+    <div
+      className="h-[15px] overflow-hidden bg-cover bg-center bg-repeat sm:h-[20px] md:h-[25px] lg:h-[30px]"
+      style={{ backgroundImage: `url(${siteBrandBar.src})` }}
+    />
   )
 }
