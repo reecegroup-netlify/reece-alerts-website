e2cd6ef8acfc9ccbe964019e1ea06b2cbc2c6c78
Adrian Beyerle <adrian.beyerle@reece.com.au>
2024-06-03 13:21:32 +1000

    fix NEXT_DEPLOY_CONTEXT env variable

--- lib/utils/getDeployContext.ts
+++ lib/utils/getDeployContext.ts
@@ -1,7 +1,7 @@
-const { NEXT_CONTEXT } = process.env
+const { NEXT_DEPLOY_CONTEXT } = process.env
 
 // get the build's deploy context
 // https://docs.netlify.com/configure-builds/environment-variables/
 export default function getDeployContext() {
-  return NEXT_CONTEXT ? NEXT_CONTEXT : 'dev'
+  return NEXT_DEPLOY_CONTEXT ? NEXT_DEPLOY_CONTEXT : 'dev'
 }
