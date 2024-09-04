f8eaefdce51a304e78cda5a958564860874ee651
Adrian Beyerle <adrian.beyerle@reece.com.au>
2024-09-05 09:51:39 +1000

    static manifest document

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,14 +1,9 @@
-import { request } from '@/lib/api/datocms'
-import { SiteManifestIconDocument } from '@/lib/api/generated'
 import { config } from '@/lib/config'
-import getSiteUrl from '@/lib/utils/getSiteUrl'
 import { MetadataRoute } from 'next'
 
 const { siteNameWithReece, siteNameWithoutReece, description } = config.site
 
-export default async function manifest(): Promise<MetadataRoute.Manifest> {
-  const { site } = await request(SiteManifestIconDocument)
-  const { mimeType, responsiveImage } = site.manifestIcon
+export default function manifest(): MetadataRoute.Manifest {
 
   return {
     background_color: '#F4F5F6',
@@ -16,15 +11,15 @@ export default async function manifest(): Promise<MetadataRoute.Manifest> {
     display: 'standalone',
     icons: [
       {
-        src: responsiveImage.src,
-        sizes: `${responsiveImage.width}x${responsiveImage.height}`,
-        type: mimeType,
+        src: 'https://www.datocms-assets.com/132344/1717408725-android-chrome-384x384.png?h=512&w=512',
+        sizes: '512x512',
+        type: 'image/png',
         purpose: 'any',
       },
       {
-        src: responsiveImage.src,
-        sizes: `${responsiveImage.width}x${responsiveImage.height}`,
-        type: mimeType,
+        src: 'https://www.datocms-assets.com/132344/1717408725-android-chrome-384x384.png?h=512&w=512',
+        sizes: '512x512',
+        type: 'image/png',
         purpose: 'maskable',
       },
     ],
