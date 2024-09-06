dcadcf94d89d88b6f18b07ff57b5a0ee3b76be11
Adrian Beyerle <adrian.beyerle@reece.com.au>
2024-09-06 14:39:13 +1000

    do not async client component

--- components/HeaderNav.tsx
+++ components/HeaderNav.tsx
@@ -4,14 +4,9 @@ import { usePathname } from 'next/navigation'
 import ButtonBack from './ButtonBack'
 import ButtonSort from './ButtonSort'
 import { Suspense } from 'react'
-import { PostsAllCountDocument } from '@/lib/api/generated'
-import { request } from '@/lib/api/datocms'
 
-export default async function HeaderNav() {
+export default function HeaderNav() {
   const pathname = usePathname()
-
-  const { postsAll } = await request(PostsAllCountDocument)
-
   return (
     <nav className="text-sm leading-snug">
       <ul className="flex space-x-4">
@@ -22,7 +17,7 @@ export default async function HeaderNav() {
             </Suspense>
           </li>
         )}
-        {postsAll.count > 0 && (pathname === '/' || pathname.startsWith('/page')) && (
+        {(pathname === '/' || pathname.startsWith('/page')) && (
           <li>
             <Suspense>
               <ButtonSort />
