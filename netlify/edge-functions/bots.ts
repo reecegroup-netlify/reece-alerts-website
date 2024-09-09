16545587b570f64722be50baea479fef79100b5a
Adrian Beyerle <adrian.beyerle@reece.com.au>
2024-09-09 13:08:00 +1000

    introduce bot blocker edge function

---
+++ netlify/edge-functions/bots.ts
@@ -0,0 +1,21 @@
+// inspired (and taken) from ethan Netlify's blog post
+// https://developers.netlify.com/guides/blocking-ai-bots-and-controlling-crawlers/
+
+import { Context, Config } from "@netlify/edge-functions";
+import agents from "../../agents.json" with { type: "json" };
+
+export default async (request: Request, context: Context) => {
+  const ua = request.headers.get('user-agent');
+  let isBot = false;
+  agents.forEach(u => {
+    if (ua.toLowerCase().includes(u.toLowerCase())) {
+      isBot = true;
+    }
+  })
+  const response = isBot ? new Response(null, { status: 401 }) : await context.next();
+  return response;
+};
+
+export const config: Config = {
+  path: "*",
+};
